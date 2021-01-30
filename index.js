663d119f883ebe3b37bf27e1b28d07f0119d2c21
Garrusir <urbansparrow1256@gmail.com>
2021-01-30 12:20:01 +0300

    add bd connect

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config.json');
 const app = express();
 const port = process.env.PORT || 3333;
 
@@ -8,12 +10,15 @@ app.use(express.json());
 app.use(cors());
 
 
+const schema = new mongoose.Schema({
+    name: {type: String, required: true},
+    message: {type: String, required: true},
+})
+const Message = mongoose.model('Message', schema);
 
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-
-const messages = [];
 
 io.on('connection', (client) => {
     client.on('message', msm => {
@@ -22,27 +27,38 @@ io.on('connection', (client) => {
     })
 });
 
-app.get('/messages/index', (req, res) => {
-    res.json({ value: messages });
+app.get('/messages/index', async (req, res) => {
+    const messageList = await Message.find({})
+    res.json({ value: messageList });
 })
 
 app.get('/', (req, res) => {
     res.json('API')
 })
 
-app.post('/messages/create', (req, res) => {
+app.post('/messages/create', async (req, res) => {
     const { message, name } = req.body;
-    const msm = {
-        message,
-        name
-    };
-    messages.push(msm);
+
+    const msm = new Message({
+        name, message
+    })
+
+    await msm.save();
+
     io.emit('message', msm);
     res.send('ok');
 })
 
+mongoose.connect(config.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+    .then(() => {
+        server.listen(port, () => console.log(`Server is running on port ${port}`));
+    })
+    .catch((e) => {
+        console.log('server error:', e.message);
+        process.exit(1);
+    });
 
-
-
-
-server.listen(port, () => console.log(`Server is running on port ${port}`));
